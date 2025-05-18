bd2c303ebee22395e862b819a09190611e3915af
Seu Nome <seu-emailgmail.com>
2025-05-18 17:29:57 +0100

    adiciona scripts e arquivos do projeto ETL

--- exemplos/exemplo2.py
+++
@@ -1,7 +0,0 @@
-print("Teste dentro do Python")
-import requests
-print("Importação feita")
-response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
-data = response.json()
-print(data)
-
